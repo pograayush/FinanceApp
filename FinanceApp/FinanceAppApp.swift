ee9890c13df0b1d6b156c12255055cad70b9e8ed
ayush pogra <ayushpogra0@gmail.com>
2024-09-23 22:46:52 +0530

    Initial Commit

---
+++ FinanceApp/FinanceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceAppApp.swift
+//  FinanceApp
+//
+//  Created by Ayush Pogra on 23/09/24.
+//
+
+import SwiftUI
+
+@main
+struct FinanceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
